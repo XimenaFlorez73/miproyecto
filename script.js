1aa5f7a5bf0c6b4cb8c64691bb3d834ea058ac4d
Ximena Florez <xameaiko@gmail.com>
2024-11-09 01:31:40 -0500

    Haciendo login

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     menu.classList.remove("visible");
 });
+
+let signUp = document.getElementById("signUp");
+let signIp = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
+
+signIp.onclick = function(){
+    nameInput.login.maxHeight = "0";
+    title.innerHTML = "login";
+    signUp.classList.add("disable");
+    signIp.classList.remove("disable");
+}
+
+signUp.onclick = function(){
+    nameInput.login.maxHeight = "60px";
+    title.innerHTML = "Registro";
+    signUp.classList.remove("disable");
+    signIp.classList.add("disable");
+}
